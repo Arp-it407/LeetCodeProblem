a70f490738d7c5d5192911c82517356e3574c322
Arpit Tiwari <tarpit923@gmail.com>
2026-07-13 07:10:55 +0530

    Time: 55 ms (87.66%), Space: 26.3 MB (89.6%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canCross(vector<int>& stones) {
+       int n = stones.size();
+       vector<vector<bool>> dp(n, vector<bool>(n+1,false));
+       dp[0][1] = true;
+
+       for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            int k = stones[i] - stones[j];
+            if(k > n || !dp[j][k])
+            continue;
+
+            if(i == n-1)
+            return true;
+            dp[i][k] = true;
+            if(k-1 >= 0) dp[i][k-1] = true;
+            if(k+1 <= n) dp[i][k+1] = true;
+        }
+       }
+       return false;
+    }
+};
